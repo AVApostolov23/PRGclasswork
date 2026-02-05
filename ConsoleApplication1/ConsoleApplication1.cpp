970678a090bbc3a513f8848840fe0cc2357ea0fa
Цветомир Петров Лучев <TSPLuchev23@codingburgas.bg>
2026-02-05 14:25:51 +0200

    new fils

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../lib/library.h"
+using namespace std;
+int main() {
+	int a = 2;
+	int b = 3;
+	cout << add1(a, b);
+ }
---
+++ lib/library.cpp
@@ -0,0 +1,5 @@
+#include "library.h"
+
+int add1(int a, int b) {
+	return a + b;
+}
